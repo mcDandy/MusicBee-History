867aa3e9faf8f96417c4af09ffc54842b6840eda
mcDandy <danda.martinek@gmail.com>
2026-04-23 22:27:21 +0200

    Genre could be usefull

--- HistoryPlugin.cs
+++ HistoryPlugin.cs
@@ -8,7 +8,37 @@ namespace MusicBeePlugin
 {
     public partial class Plugin
     {
+        public void ReceiveNotification(NotificationType type)
+        {
+            if (type == NotificationType.PlayStateChanged)
+            {
+                var state = mbApiInterface.Player_GetPlayState();
+                int played = mbApiInterface.Player_GetPosition();
+                var artist = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
+                var album = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Album);
+                var title = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.TrackTitle);
+                var genre = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Genre);
 
+                if (state == PlayState.Stopped)
+                {
+
+                }
+                else if (state == PlayState.Paused)
+                {
+                }
+                else if (state == PlayState.Playing)
+                {
+                }
+            }
+            else if (type == NotificationType.TrackChanged)
+            {
+
+            }
+            else if (type == NotificationType.TrackChanging)
+            {
+
+            }
+        }
         private void InitDatabase()
         {
             using(var conn = new SqliteConnection("Data Source=MusicBeeHistory.db"))
@@ -22,11 +52,14 @@ namespace MusicBeePlugin
                     Artist integer,
                     Album integer,
                     Title integer,
+                    Genre integer,
                     Action integer,
+                    Played float,
                     Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                     foreign key(Artist) references Artists(Id),
                     foreign key(Album) references Albums(Id),
-                    foreign key(Title) references Titles(Id)
+                    foreign key(Title) references Titles(Id),
+                    foreign key(Genre) references Genres(Id),
                     foreign key(Action) references Actions(Id)
                 )";
                 command.ExecuteNonQuery();
@@ -41,6 +74,11 @@ namespace MusicBeePlugin
                 )";
                 command.ExecuteNonQuery();
                 command.CommandText = @"CREATE TABLE IF NOT EXISTS Titles (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT UNIQUE
+                )";
+                command.ExecuteNonQuery();
+                command.CommandText = @"CREATE TABLE IF NOT EXISTS Genres (
                     Id INTEGER PRIMARY KEY AUTOINCREMENT,
                     Name TEXT UNIQUE
                 )";
