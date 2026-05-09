d58b92f5a096bd2a980ad48eacd1320d4a45d785
mcDandy <danda.martinek@gmail.com>
2026-05-09 23:54:39 +0200

    Fix SQL

--- HistoryControl.cs
+++ HistoryControl.cs
@@ -26,19 +26,23 @@ namespace MusicBeePlugin
             try
             {
                 var sql = @"
-                            SELECT 
-                                tr.Artist_Id, 
-                                (h.Played - COALESCE(LAG(h.Played) OVER (PARTITION BY h.Track_Id ORDER BY h.Id), 0)) * 
-                                ((100.0 + h.speed) / 100.0) * ((100.0 + h.sample_rate) / 100.0) as Time_played,
-                                h.Time
-                            FROM History h
-                            JOIN Tracks tr ON h.Track_Id = tr.Id
-                            WHERE (h.event_type = 16 OR (h.event_type = 2 AND h.player_state IN (6, 7)))
-                        ) h
-                        JOIN Artists a ON h.Artist_Id = a.Id
-                        WHERE h.Time_played > 0 
-                        GROUP BY a.Value
-                        ORDER BY Listen_time DESC;";
+SELECT 
+    a.Value as Artist,
+    ROUND(SUM(h.min) / 60000.0, 2) as MinutesPlayed
+FROM (
+    SELECT 
+        tr.Artist_Id, 
+        (h.Played - COALESCE(LAG(h.Played) OVER (PARTITION BY h.Track_Id ORDER BY h.Id), 0)) * 
+        ((100.0 + h.speed) / 100.0) * ((100.0 + h.sample_rate) / 100.0) as min,
+        h.Time
+    FROM History h
+    JOIN Tracks tr ON h.Track_Id = tr.Id
+    WHERE (h.event_type = 16 OR (h.event_type = 2 AND h.player_state IN (6, 7)))
+) h
+JOIN Artists a ON h.Artist_Id = a.Id
+WHERE h.min > 0 
+GROUP BY a.Value
+ORDER BY MinutesPlayed DESC;";
 
                 var table = new DataTable();
                 using (var conn = new SQLiteConnection($"Data Source={_dbPath};Version=3;"))
