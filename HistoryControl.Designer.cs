5e054383fb730dd59e2afba76f04e91966f3790a
mcDandy <danda.martinek@gmail.com>
2026-05-11 22:15:12 +0200

    Update HistoryControl.Designer.cs

--- HistoryControl.Designer.cs
+++ HistoryControl.Designer.cs
@@ -58,7 +58,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(142, 124);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Most Played Artists";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
@@ -81,7 +81,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(142, 124);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Top Tracks";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // HistoryControl
