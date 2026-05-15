7c4b61edc828d0817cc2954637a2e93774af555e
mcDandy <danda.martinek@gmail.com>
2026-05-15 13:19:16 +0200

    change the SQL

--- HistoryControl.cs
+++ HistoryControl.cs
@@ -65,11 +65,11 @@ namespace MusicBeePlugin
             {
                 string sql = @"
                             SELECT 
-                                a.Value as Artist,
+                                a.Value as Artist, t.Value as Title,
                                 ROUND(SUM(h.min) / 60000.0, 2) as MinutesPlayed
                             FROM (
                                 SELECT 
-                                    tr.Artist_Id, 
+                                    tr.Artist_Id, tr.Title_id, 
                                     (h.Played - COALESCE(LAG(h.Played) OVER (PARTITION BY h.Track_Id ORDER BY h.Id), 0)) / 
                                     ((100.0 + h.speed) / 100.0) * ((100.0 + h.sample_rate) / 100.0) as min,
                                     h.Time
@@ -78,8 +78,9 @@ namespace MusicBeePlugin
                                 WHERE (h.event_type = 16 OR (h.event_type = 2 AND h.player_state IN (6, 7)))
                             ) h
                             JOIN Artists a ON h.Artist_Id = a.Id
+                            JOIN Titles t ON h.Title_id = t.Id
                             WHERE h.min > 0 
-                            GROUP BY a.Value
+                            GROUP BY a.Value, t.Value
                             ORDER BY MinutesPlayed DESC;";
 
                 var table = new DataTable();
